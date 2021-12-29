de7d3f7722c086f5f7b873ea04184097ed8c667d
hitokun-s <jgpuauno@gmail.com>
2021-12-29 14:08:25 +0900

    add vue-gtag for google analytics (test)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 // register vue composition api globally
-import { ViteSSG } from 'vite-ssg'
+import {ViteSSG} from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
-import { setupLayouts } from 'virtual:generated-layouts'
+import {setupLayouts} from 'virtual:generated-layouts'
 import App from './App.vue'
+// import VueGtag from "vue-gtag-next";
+import VueGtag from "vue-gtag";
 
 // your custom styles here
 // import './styles/main.css'
@@ -10,12 +12,22 @@ import "../src/assets/main.scss"
 
 const routes = setupLayouts(generatedRoutes)
 
+// app.use(VueGtag, {
+//     property: {
+//         id: "UA-123456-7"
+//     }
+// });
+
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
-  App,
-  { routes:routes },
-  (ctx) => {
-    // install all modules under `modules/`
-    Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
-  },
+    App,
+    {routes: routes},
+    (ctx) => {
+        ctx.app.use(VueGtag, {
+            config: {id: "UA-48867719-1"},
+            property: {id: "UA-48867719-1"}
+        });
+        // install all modules under `modules/`
+        Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
+    },
 )
